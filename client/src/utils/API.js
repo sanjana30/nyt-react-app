9d2a9294230d9f5686d5b1e572587726c729a9ab
Muggle Potter <sanjanatripathi007@gmail.com>
2018-09-25 11:08:49 -0500

    changes made in api call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,7 @@ export default {
   // Gets all books
   searchArticle: function(searchterm, startdate, enddate) { 
     const apikey = "02e5a12e46bd44faa379b80fbed7c6c0";   
-    return axios.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchterm+"&begin_date="+startdate+"&end_date="+enddate+"&api-key="+apikey);
+    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchterm+"&begin_date="+startdate+"&end_date="+enddate+"&api-key="+apikey);
   },
  
   // getBook: function(id) {
